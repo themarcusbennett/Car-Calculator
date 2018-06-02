98701913a04985113e3b1cec800c44a06ce261a9
Marcus Bennett <marcus@minimalmar.com>
2018-06-02 15:21:07 -0400

    Initial Commit

---
+++ intersect.js
@@ -0,0 +1,13 @@
+var nav = document.getElementById("sidenav");
+function toggleSidenav(){
+   nav.classList.toggle('active');
+}
+/*var monthlyPayment = function (car, month) {
+    month = prompt("How many months would you like to calculate this span over?");
+	var gas = prompt("How much money do you spend on gas a month?");
+	var carNote = prompt("How much is your monthly car note? If not applicable enter 0");
+	var insurance = prompt("How much is your monthly insurance?");
+	car = parseInt(gas) + parseInt(carNote) + parseInt(insurance);
+	alert("You will pay " + car * month + " dollars to keep this car for " + month + " months.");
+}*/
+
